db09bc91c7b6285725a39fcdf008a25e8a9e0844
Klayane <Klayane@gmail.com>
2020-06-09 20:44:09 -0300

    Arrumei o problema de não mostrar o resultado

--- processa.php
+++ processa.php
@@ -1,9 +1,9 @@
 <?php
 
 	// Entrada
-	$valor1 = $_GET['valor1'];
-	$valor2 = $_GET['valor2'];
-	$operacao = $_GET['operacao'];
+	$valor1 = $_POST['valor1'];
+	$valor2 = $_POST['valor2'];
+	$operacao = $_POST['operacao'];
 
 	// Processamento
 	switch($operacao) {
@@ -16,7 +16,7 @@
 				  echo "Resultado: $resultado";
 				  break;
 		
-		case '*' $resultado = $valor1 * $valor2;
+		case '*': $resultado = $valor1 * $valor2;
 				 echo "Resultado: $resultado";
 				 break;
 	}
